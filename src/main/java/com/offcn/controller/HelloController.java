72d87fcb1a0d6695ffade145fb22603812e02a1e
offcn-java <1208877605@qq.com>
2020-11-09 19:31:55 +0800

    第二个

---
+++ src/main/java/com/offcn/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.offcn.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("getname")
+    @ResponseBody
+    public String demo(){
+       String name= SecurityContextHolder.getContext().getAuthentication().getName();
+       return name;
+    }
+}
+
